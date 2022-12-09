9b7abdc0dc609b9372a13e349291603f63ede85a
yurii19 <yurikolechkin@gmail.com>
2022-12-09 10:05:52 +0200

    services created

---
+++ src/app/cats.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CatsService {
+
+  API_KEY = 'live_cnlTmK2t9iMi864zh29DVkXA8FoY3CIE0l2gDfEn5nXCvoYIL0ah7Sbon5FJAPW3';
+  BASE_URL = 'https://api.thecatapi.com/v1'
+  //BASE_URL = 'https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=REPLACE_ME'
+
+  constructor(private http: HttpClient) { }
+
+  getCats(params : any =''): any{
+   return this.http.get(`${this.BASE_URL}/images/search?${params}api_key=${this.API_KEY}`)
+  }
+  getBreeds(params : any ='breeds'): any{
+   return this.http.get(`${this.BASE_URL}/breeds`)
+  }
+
+}
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,13 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { CatsService } from 'src/app/cats.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
+  
 })
 export class MainComponent implements OnInit {
 
+  filters: FormGroup = new FormGroup({
+    limit:  new FormControl(10),
+    breeds:  new FormControl(),
+  })
+
+ 
+
+  cats : any[]= [ 'murka', 'katika', 'rudyk']
+
+ cats$: any = this.catService.getCats('limit=10&')
+
+  constructor(
+    private catService: CatsService
+  ){}
+
   ngOnInit(): void {
+  //this.cats$ = this.catService.getCats('limit=10&')
+  this.catService.getBreeds().subscribe((d: any) => console.log(d))
+  //this.cats$.subscribe((d: any)  => console.log(d))
     console.log('ngOninit')
   }
 
